10cc109790c2deb6ac4e9894fafa076ea2b44e74
alroiz <alroiz@gmail.com>
2015-05-11 14:53:35 +0200

    "configjs"

--- config/env/development.js
+++ config/env/development.js
@@ -4,5 +4,6 @@ module.exports = {
 	port: port,
 	//db: 'mongodb://localhost/statsPortalCVTE',
 	db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/one5Tevu',
-	socketServer: 'http://localhost:3002'
+	//socketServer: 'http://localhost:3002'
+	socketServer: 'https://dashboardcvte.herokuapp.com/'
 };
