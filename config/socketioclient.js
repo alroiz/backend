29015cf6e25ee09be65a76dac470db06e5cf95a9
alroiz <alroiz@gmail.com>
2015-05-11 15:17:32 +0200

    socketserver

--- config/socketioclient.js
+++ config/socketioclient.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client')
 //var io = require('socket.io')
 module.exports = function() {
-	var socket = io.connect('http://localhost:3002', {reconnect: true});
+	var socket = io.connect('https://dashboardcvte.herokuapp.com/', {reconnect: true});
 	socket.on('connect', function(socketOpen) { 
     	console.log('Me he conectado a http://localhost:3002!');
     	
