164d6e7c48d1fb29d53316c343b452bc1bce1324
alroiz <alroiz@gmail.com>
2015-05-18 11:13:31 +0200

    aggregatedmodels

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,8 @@ var port = 3000;
 
 module.exports = {
 	port: port,
-	db: 'mongodb://localhost/statsPortalCVTE',
-	//db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/one5Tevu',
-	socketServer: 'http://localhost:3002'
-	//socketServer: 'https://dashboardcvte.herokuapp.com/'
+	//db: 'mongodb://localhost/statsPortalCVTE',
+	db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/one5Tevu',
+	//socketServer: 'http://localhost:3002'
+	socketServer: 'https://dashboardcvte.herokuapp.com/'
 };
