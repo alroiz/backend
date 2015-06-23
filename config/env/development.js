3bc3a8c85906a35365ceca6142e1dc08169f25d8
alroiz <alroiz@gmail.com>
2015-06-23 11:10:05 +0200

    first commit

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,11 @@ var port = 3000;
 module.exports = {
 	port: port,
 	//db: 'mongodb://localhost/statsPortalCVTE',
-	db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/one5Tevu',
+	//db: 'mongodb://magicbox:magicbox@proximus.modulusmongo.net:27017/yqA8xyhu',
+	//db: 'mongodb://magicbox:magicbox@apollo.modulusmongo.net:27017/gUz3uvob',
+	//db: 'mongodb://magicbox:magicbox@ds045011.mongolab.com:45011/heroku_4sxw2p0h',
+	//db: 'mongodb://heroku_4sxw2p0h:8h4k83svcg393idrrlq5ue48it@ds045011.mongolab.com:45011/heroku_4sxw2p0h',
 	//socketServer: 'http://localhost:3002'
-	socketServer: 'https://dashboardcvte.herokuapp.com/'
+	db: 'mongodb://magicbox:magicbox@ds045011.mongolab.com:45011/heroku_4sxw2p0h',
+	socketServer: 'https://magicdashboard.herokuapp.com/'
 };
